e0c59c00ae99b0637c1f9f61618d7d1fafe4c63c
adebkowski <adam.debkowski@nordcloud.com>
2021-11-21 19:36:42 +0100

    Added comments to error.go

--- v2/errors/error.go
+++ v2/errors/error.go
@@ -1,5 +1,8 @@
 package errors
 
+// NCError is the error type that can store additional message, fields and stack trace.
+// It should be used as a standard error.
+// If more information about error is required, call Info(err).
 type NCError struct {
 	err error
 
@@ -10,6 +13,7 @@ type NCError struct {
 	funcName   string
 }
 
+// Error returns error message based on all wrapped errors.
 func (e NCError) Error() string {
 	if e.err != nil {
 		return e.message + ": " + e.err.Error()
@@ -18,6 +22,8 @@ func (e NCError) Error() string {
 	return e.message
 }
 
+// Info returns info about error.
+// It should not be used directly. Use Info(err) instead.
 func (e NCError) Info() Info {
 	return Info{
 		Message:    e.message,
@@ -27,14 +33,18 @@ func (e NCError) Info() Info {
 	}
 }
 
+// Unwrap unwraps the error.
+// It implements the errors.Unwrap interface
 func (e NCError) Unwrap() error {
 	return e.err
 }
 
+// StackTrace returns stack trace of an error.
 func (e NCError) StackTrace() StackTrace {
 	return e.stackTrace
 }
 
+// New creates a new instance of NCError.
 func New(message string, fields Fields) NCError {
 	var funcName string
 
@@ -78,6 +88,8 @@ func NewWithErr(err error, message string, fields Fields) NCError {
 	}
 }
 
+// Wrap lets you to wrap an error with specyfing message and fields.
+// If the err is nil, then the returned error is nil as well.
 func Wrap(err error, message string, fields Fields) error {
 	if err == nil {
 		return nil
@@ -103,6 +115,9 @@ func Wrap(err error, message string, fields Fields) error {
 	}
 }
 
+// W lets you to wrap an error without specyfing message or fields.
+// The message will be taken from the function name of the function where W is called.
+// The fields will remain empty.
 func W(err error) error {
 	if err == nil {
 		return nil
@@ -130,10 +145,10 @@ func W(err error) error {
 }
 
 func errorHasStackTrace(err error) bool {
-	var st StackTracer
+	var st stackTracer
 	return As(err, &st)
 }
 
-type StackTracer interface {
+type stackTracer interface {
 	StackTrace() StackTrace
 }
